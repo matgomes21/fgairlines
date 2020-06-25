37f18a67a5b6d1afb6b3680aba8e7a03bf537a5c
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-06-25 00:57:55 -0300

    Implemented new flight functionality

--- frontend/src/pages/NewFlight/index.js
+++ frontend/src/pages/NewFlight/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg'
 
 export default function NewFlight() {
+    const [destiny, setDestiny] = useState('');
+    const [data, setData] = useState('');
+    const [hour, setHour] = useState('');
+    const [value, setValue] = useState('');
+
+    const history = useHistory();
+
+    const airlineId = localStorage.getItem('airlineId');
+
+    async function handleNewFlight(e) {
+        e.preventDefault();
+
+        const dataFlight = {
+            destiny,
+            data,
+            hour,
+            value,
+        };
+
+        try {
+            await api.post('flights', dataFlight, {
+                headers: {
+                    Authorization: airlineId,
+                }
+            })
+
+            history.push('/profile');
+        } catch(err) {
+            alert('Erro ao criar novo voo, tente novamente.');
+        }
+    }
+
     return (
         <div className="new-flight-container">
             <div className="content">
@@ -16,20 +50,36 @@ export default function NewFlight() {
                     <h1>Cadastrar novo voo</h1>
                     <p>Cadastre um novo voo e coloque-o para venda agora mesmo!</p>
 
-                    <Link className="back-link" to="/flights">
+                    <Link className="back-link" to="/profile">
                         <FiArrowLeft size={16} color="#17333C" />
                         Voltar
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Destino" />
+                <form onSubmit={handleNewFlight}>
+                    <input 
+                        placeholder="Destino" 
+                        value={destiny}
+                        onChange={e => setDestiny(e.target.value)}
+                    />
                     <div className="form-group">
-                        <input placeholder="Data" />
-                        <input placeholder="Hora" />
+                        <input 
+                            placeholder="Data" 
+                            value={data}
+                            onChange={e => setData(e.target.value)}
+                        />
+                        <input 
+                            placeholder="Hora" 
+                            value={hour}
+                            onChange={e => setHour(e.target.value)}
+                        />
                     </div>
-                    <input placeholder="Valor (em reais)" />
-                    <button type="button">Cadastrar</button>
+                    <input 
+                        placeholder="Valor (em reais)" 
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
+                    <button className="button" type="submit">Cadastrar</button>
                     
                         
                 </form>
