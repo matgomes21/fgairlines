8d4025268c0bba7888719e1831ec81ec62e64900
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-06-25 00:16:25 -0300

    Implemented functionality that lists the flights of a logged airline

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
 
 export default function Profile() {
+    const [flights, setFlights] = useState([]);
+
+    const airlineId = localStorage.getItem('airlineId');
+
+    useEffect(() => {
+        api.get('profile', {
+            headers: {
+                Authorization: airlineId,
+            }
+        }).then(response => {
+            setFlights(response.data);
+        })
+    },[airlineId]);
+
     return(
         <div className="flights-container">
             <header>
@@ -21,62 +37,22 @@ export default function Profile() {
             <h1>Voos Disponíveis</h1>
 
             <ul>
-                <li>
-                    <strong>DESTINO:</strong>
-                    <p>Destino teste</p>
+                {flights.map(flight => (
+                    <li key={flight.id}>
+                        <strong>DESTINO:</strong>
+                        <p>{flight.destiny}</p>
 
-                    <strong>DATA/HORA:</strong>
-                    <p>Maia corno</p>
+                        <strong>DATA/HORA:</strong>
+                        <p>{flight.data} - {flight.hour}</p>
 
-                    <strong>PREÇO:</strong>
-                    <p>R$ 777,00</p>
+                        <strong>PREÇO:</strong>
+                        <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(flight.value)}</p>
 
-                    <button type="button">
-                        <FiTrash2 size={20} color="#17333C" />
-                    </button>
-                </li>
-                <li>
-                    <strong>DESTINO:</strong>
-                    <p>Destino teste</p>
-
-                    <strong>DATA/HORA:</strong>
-                    <p>Maia corno</p>
-
-                    <strong>PREÇO:</strong>
-                    <p>R$ 777,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#17333C" />
-                    </button>
-                </li>
-                <li>
-                    <strong>DESTINO:</strong>
-                    <p>Destino teste</p>
-
-                    <strong>DATA/HORA:</strong>
-                    <p>Maia corno</p>
-
-                    <strong>PREÇO:</strong>
-                    <p>R$ 777,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#17333C" />
-                    </button>
-                </li>
-                <li>
-                    <strong>DESTINO:</strong>
-                    <p>Destino teste</p>
-
-                    <strong>DATA/HORA:</strong>
-                    <p>Maia corno</p>
-
-                    <strong>PREÇO:</strong>
-                    <p>R$ 777,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#17333C" />
-                    </button>
-                </li>
+                        <button type="button">
+                            <FiTrash2 size={20} color="#17333C" />
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
     );
