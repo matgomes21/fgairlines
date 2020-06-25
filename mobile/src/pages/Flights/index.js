998bb6bf73e1017efe780357d887cc4dc94a028f
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-06-25 07:38:21 -0300

    Created and styled flights page

--- mobile/src/pages/Flights/index.js
+++ mobile/src/pages/Flights/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 
 import logo from '../../assets/logo.png';
 
 import styles from './styles';
 
 export default function Flights() {
+    const navigation = useNavigation();
+
+    function navigateToDetail() {
+        navigation.navigate('Detail');
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -17,6 +25,33 @@ export default function Flights() {
 
             <Text style={styles.title}>Bem-vindo!</Text>
             <Text style={styles.description}>Escolha um dos voos abaixo e boa viagem.</Text>
+
+            <FlatList
+                style={styles.flightList}
+                data={[1, 2, 3]}
+                keyExtractor={flight => String(flight)}
+                showsVerticalScrollIndicator={false}
+                renderItem={() => (
+                    <View style={styles.flight}>
+                        <Text style={styles.flightProperty}>AIRLINE:</Text>
+                        <Text style={styles.flightValue}>Maia</Text>
+
+                        <Text style={styles.flightProperty}>DESTINO:</Text>
+                        <Text style={styles.flightValue}>São Paulo</Text>
+
+                        <Text style={styles.flightProperty}>DATA/HORA:</Text>
+                        <Text style={styles.flightValue}>30/06 - 14:00</Text>
+
+                        <Text style={styles.flightProperty}>PREÇO:</Text>
+                        <Text style={styles.flightValue}>R$ 220,00</Text>
+
+                        <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetail}>
+                            <Text style={styles.detailsButtonText}>Entrar em contato</Text>
+                            <Feather name="arrow-right" size={16} color='#17333C' />
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
         </View>
     );
 }
--- mobile/src/pages/Flights/styles.js
+++ mobile/src/pages/Flights/styles.js
@@ -1,5 +1,77 @@
 import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
 export default StyleSheet.create({
-    container: {}
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 20,
+        backgroundColor: '#ADD8E6'
+    },
+
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+
+    headerText: {
+        fontSize: 15,
+        color: '#737380',
+    },
+
+    headerTextBold: {
+        fontWeight: 'bold'
+    },
+
+    title: {
+        fontSize: 30,
+        marginBottom: 16,
+        marginTop: 48,
+        color: '#17333C',
+        fontWeight: 'bold'
+    },
+
+    description: {
+        fontSize: 16,
+        lineHeight: 24,
+        color: '#737380'
+    },
+
+    incidentList: {
+        marginTop: 32,
+
+    },
+
+    flight: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#ffffff',
+        marginBottom: 16,
+    },
+
+    flightProperty: {
+        fontSize: 14,
+        color: '#41414d',
+        fontWeight: 'bold',
+    },
+
+    flightValue: {
+        marginTop: 8,
+        fontSize: 15,
+        marginBottom: 24,
+        color: '#737380'
+    },
+
+    detailsButton: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+
+    detailsButtonText: {
+        color: '#17333C',
+        fontSize: 15,
+        fontWeight: 'bold',
+    }
 });
