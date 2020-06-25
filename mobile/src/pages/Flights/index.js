413791cb3f7b85b9f6336e96c7360fe82be2064e
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-06-25 06:56:15 -0300

    Created a variable to return the total flights registered on backend and started to make flights page on mobile

--- mobile/src/pages/Flights/index.js
+++ mobile/src/pages/Flights/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text } from 'react-native';
+
+import logo from '../../assets/logo.png';
+
+import styles from './styles';
 
 export default function Flights() {
     return (
-        <View />
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logo} />
+                <Text style={styles.headerText}>
+                    Total de <Text style={styles.headerTextBold}>0 voos</Text>.
+                </Text>
+            </View>
+
+            <Text style={styles.title}>Bem-vindo!</Text>
+            <Text style={styles.description}>Escolha um dos voos abaixo e boa viagem.</Text>
+        </View>
     );
 }
