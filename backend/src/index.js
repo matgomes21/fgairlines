0d03d50f0cdd83c0f7c1be9268d44d7be43d8147
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-06-24 22:40:49 -0300

    Installed axios and connected register page with backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
+const cors =  require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-
 app.listen(3333);
-
-/**
- * Funcionalidades:
- *  - Login de linha aerea
- *  - Cadastro de linha aerea
- *  - Logout
- *  - Cadastro do voos especificos
- *  - Listar os voos especificos
- *  - Deletar voos
- */
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import api from '../../services/api';
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
 
 export default function Register() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
+    const history = useHistory();
+
+    async function handleRegister(e){
+        e.preventDefault();
+
+        const data = {
+            name,
+            email,
+            city,
+            uf,
+        }
+
+        try{
+            const response = await api.post('airlines', data);
+            alert(`ID de acesso: ${response.data.id}`);
+            history.push('/');
+        }catch(err){
+            alert('Erro no cadastro, tente novamente.');
+        }
+    }
+
     return (
         <div className="register-container">
             <div className="content">
@@ -22,12 +49,24 @@ export default function Register() {
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Nome da linha aérea" />
-                    <input type="email" placeholder="E-mail" />
-                    <input placeholder="Cidade" />
+                <form onSubmit={handleRegister}>
+                    <input placeholder="Nome da linha aérea" 
+                        value={name}
+                        onChange={e=>setName(e.target.value)}
+                    />
+                    <input type="email" placeholder="E-mail" 
+                        value={email}
+                        onChange={e=>setEmail(e.target.value)}
+                    />
+                    <input placeholder="Cidade" 
+                        value={city}
+                        onChange={e=>setCity(e.target.value)}
+                    />
                     <div className="form-group">
-                        <input placeholder="UF" style={{ width: 80 }} />
+                        <input placeholder="UF" style={{ width: 80 }} 
+                            value={uf}
+                            onChange={e=>setUf(e.target.value)}
+                        />
                         <button type="submit">Cadastrar</button>
                     </div>
                     
