cf5f4942f6ba11ae36e23a6a63881a2b89faa7f9
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-06-29 22:58:42 -0300

    Added password field to airline table

--- backend/src/database/migrations/20200622151721_create_airline.js
+++ backend/src/database/migrations/20200622151721_create_airline.js
@@ -5,6 +5,7 @@ exports.up = function(knex) {
     table.string('id').primary();
     table.string('name').notNullable();
     table.string('email').notNullable();
+    table.string('password').notNullable();
     table.string('city').notNullable();
     table.string('uf', 2).notNullable();
   });
